bc5feeb9c2753b9082ccb88984e5721bab535751
raju <>
2025-02-27 22:30:35 +0530

    comments added

--- calculator.py
+++ calculator.py
@@ -1,17 +1,19 @@
 def add(numbers:int)->int:
+    # return 0 if numbers get empty string
     if numbers == "":
         return 0
-    if numbers.startswith("//"):
-        unpack = numbers.split("\n",1)
-        delimiter = unpack[0][2]
-        numbers = unpack[1]
+    if numbers.startswith("//"): #checking delimiter 
+        unpack = numbers.split("\n",1) # splitting delimter adn numbers
+        delimiter = unpack[0][2] # delimter
+        numbers = unpack[1] # numbers
         num_list = numbers.split(delimiter)
     else:
-        numbers = numbers.replace("\n",",")
-        num_list= numbers.split(",")
+        numbers = numbers.replace("\n",",") # new line as delimter
+        num_list= numbers.split(",") #delimiter as ,
     numbers = [int(n) for n in num_list]
-    negatives = [n for n in numbers if n < 0]
+    negatives = [n for n in numbers if n < 0] #checking negatives
     if negatives:
-        raise ValueError(f"negative numbers not allowed {', '.join(map(str, negatives))}")
-    allowed_nums = [n for n in numbers if n <= 1000]
-    return sum(allowed_nums)
+        raise ValueError(f"negative numbers not allowed {', '.join(map(str, negatives))}") # showing negative numbers
+    allowed_nums = [n for n in numbers if n <= 1000] # checking positive numbers
+
+    return sum(allowed_nums) # adding all string numbers
--- test_calculator.py
+++ test_calculator.py
@@ -3,34 +3,34 @@ from calculator import add
 import pytest
 
 def test_empty_string():
-    assert add("") == 0
+    assert add("") == 0 # test case for zerp
 
 def test_string_return_single_number():
-    assert add("1") == 1
+    assert add("1") == 1 # test case for single number
 
 
 def test_two_number_string():
-    assert add("1,5") == 6
+    assert add("1,5") == 6 # test case for two numbers
 
 
 def test_multiple_number_string():
-    assert add("1,5,9") == 15
+    assert add("1,5,9") == 15 # test case for multiple numbers
 
 def test_new_lines_as_delimiter():
-    assert add("1\n2\n3") == 6
+    assert add("1\n2\n3") == 6 # test case for new line
 
 def test_custom_delimters():
-    assert add("//;\n1;2") == 3
+    assert add("//;\n1;2") == 3 # test case for custom delimter
 
 def test_negative_numbers_throw_an_exception():
     with pytest.raises(ValueError,match="negative numbers not allowed -2"):
-        add("1,-2")
+        add("1,-2") # test case for negative numbers
 
 def test_multi_negative_numbers_show():
     with pytest.raises(ValueError,match="negative numbers not allowed -1, -2"):
-        add("-1,-2,3")
+        add("-1,-2,3") # test case for multi negative numbers
 def test_ignore_if_numbers_a_1000():
-    assert add("1,1001") == 1
+    assert add("1,1001") == 1 # test case for ignoring above 1000
 
 # def test_custom_multiple_delimiter():
-#     assert add("//[*][%]\n1*2%3") == 6
+#     assert add("//[*][%]\n1*2%3") == 6 # test case for multiple delimiter
